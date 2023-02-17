01cb4d107aaa993a9a5ca7ea645e81262c86e003
SeregyVashchenkoIOS <vashhenkoBS@gmail.com>
2023-02-17 23:34:42 +0100

    Initial Commit

---
+++ HM.sprint3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HM.sprint3
+//
+//  Created by Сергей Ващенко on 17.02.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
