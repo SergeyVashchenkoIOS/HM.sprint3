b39585cc10b453417c5f6cf8c3e6c4b54ef6aa02
SeregyVashchenkoIOS <vashhenkoBS@gmail.com>
2023-02-18 11:10:09 +0100

    Экран

--- HM.sprint3/ViewController.swift
+++ HM.sprint3/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lableScore: UILabel!
+    @IBOutlet weak var buttonClick: UIButton!
+    
+    var counter: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        lableScore.text = "Значение счётчика: 0"
+        lableScore.textColor = .blue
+        buttonClick.setTitle("Кликай!", for: .normal)
+        buttonClick.backgroundColor = .darkGray
     }
 
+    @IBAction func buttonUpInsideClick(_ sender: Any) {
+        counter = counter + 1
+        lableScore.text = String("Значение счетчика: \(counter)")
+        print("Значение счётчика: \(counter)")
+    }
 
 }
 
